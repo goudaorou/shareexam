774290501b17e59bffdf40eaa279fcc41b07c779
goudaorou <orougouda@gmail.com>
2025-04-17 14:04:20 +0200

    [ADD] api shareexam avec documentation realiser avec swagger

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const app = express();
-const Port= 5000;
+const port = 5000;
+
+// Connexion à la base de données
 connectDB();
+
+// Middleware pour lire le body
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-app.use("/epreuve", require("./routes/epreuveroute"))
-app.use("/api/auth", require("./routes/userroute"))
+app.use(express.urlencoded({ extended: false }));
 
+// Routes principales
+app.use("/epreuve", require("./routes/epreuveroute"));
+app.use("/api/auth", require("./routes/userroute"));
 
+// Swagger Docs
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.listen(Port, ()=> console.log(`le serveur est démarré sur le port ${Port}`));
+// Lancement du serveur
+app.listen(port, () => console.log(`✅ Serveur démarré sur le port ${port}`));
